76972bded242c55f0fe796cc03bbce5fc6ea304a
Chip Bell <cbell@pbs.org>
2015-09-14 13:11:38 -0400

    Stubbing out the file

---
+++ app.js
@@ -0,0 +1,44 @@
+var Cell = function(object) {
+  this.marked = false;
+  this.$el = $('<div/>');
+  this.object = object;
+
+  this.$el.on('click', function() {
+    this.marked = !this.marked;
+  }.bind(this));
+};
+
+Cell.prototype.render = function($parent) {
+  this.$el.addClass('cell');
+  if(this.marked) {
+    this.$el.addClass('marked');
+  }
+
+  this.$el.html(this.object.name);
+
+  this.$el.appendTo($parent);
+};
+
+var CellContainer = function(objects) {
+  this.cells = objects.map(function(objectRow) {
+    return objectRow.map(function(object) {
+      return new Cell(object);
+    });
+  });
+};
+
+CellContainer.prototype.render = function($parent) {
+  $parent.empty();
+
+  this.cells.forEach(function(cellRow) {
+    var $row = $('<div/>').addClass('row');
+    cellRow.forEach(function(cell) {
+      cell.render($row);
+    });
+    $row.appendTo($parent);
+  });
+};
+
+$(function() {
+  new CellContainer($('#board')).render();
+});
